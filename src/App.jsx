aa85455ae913ffeacff276c41c255482111d0d0f
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-27 16:06:14 +0100

    use flags instead of original_language string

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 function App() {
 
 
-  const [query, setQuery] = useState([]);
+  const [query, setQuery] = useState('');
   const [results, setResults] = useState([]);
 
   // chiamata API movies
@@ -33,12 +33,36 @@ function App() {
       </div>
 
       <div>
+        {results.length > 0 && <h2>Movies:</h2>}
         {/* Mostra i risultati */}
         {results.map((movie) => (
           <div key={movie.id}>
             <h2>{movie.title}</h2>
             <p>{movie.original_title}</p>
-            <p>{movie.original_language}</p>
+
+            {movie.original_language === 'en' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/gb.svg" alt="Flag of English" />
+            )}
+            {movie.original_language === 'it' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/it.svg" alt="Bandiera italiana" />
+            )}
+            {movie.original_language === 'es' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/es.svg" alt="Bandera española" />
+            )}
+            {movie.original_language === 'fr' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/fr.svg" alt="Drapeau français" />
+            )}
+            {movie.original_language === 'de' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/de.svg" alt="Deutsche Flagge" />
+            )}
+            {movie.original_language === 'ja' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/jp.svg" alt="Deutsche Flagge" />
+            )}
+            {!movie.original_language && (
+              <p>{movie.original_language}</p>
+            )}
+
+
             <p>{movie.vote_count}</p>
           </div>
         ))}
