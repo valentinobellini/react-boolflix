3556c5643731c0fbca89dcd7a8e5c0681075b264
ValentinoBellini <valentinobellini.dev@gmail.com>
2025-02-27 16:32:34 +0100

    create API call for TV series

--- src/App.jsx
+++ src/App.jsx
@@ -5,21 +5,32 @@ function App() {
 
 
   const [query, setQuery] = useState('');
-  const [results, setResults] = useState([]);
+  const [movies, setMovies] = useState([]);
+  const [series, setSeries] = useState([]);
+
 
   // chiamata API movies
   function fetchResults() {
-    axios.get(`https://api.themoviedb.org/3/search/movie?api_key=95f177b1b95405d3218e218ecc202828&query=${query}`)
-      .then((res) => {
-        setResults(res.data.results)
-        setQuery('');
-      })
-      .catch(err => console.log(err));
-  };
+
+    const movieRequest = axios.get(`https://api.themoviedb.org/3/search/movie?api_key=33dd7e4bd707ec9911740ceb7d4fa3c7&language=it_IT&query=${query}`);
+    const tvRequest = axios.get(`https://api.themoviedb.org/3/search/tv?api_key=33dd7e4bd707ec9911740ceb7d4fa3c7&language=it_IT&query=${query}`);
+
+    axios.all([movieRequest, tvRequest])
+      .then(axios.spread((movieRes, tvRes) => {
+        setMovies(movieRes.data.results);
+        setSeries(tvRes.data.results);
+        setState('');
+      }))
+      .catch((err) => {
+        console.log(err);
+      });
+  }
 
 
   return (
     <>
+
+      {/* render movies list */}
       <h1>hello world</h1>
 
       <div>
@@ -33,9 +44,9 @@ function App() {
       </div>
 
       <div>
-        {results.length > 0 && <h2>Movies:</h2>}
+        {movies.length > 0 && <h2 className='list-title' >Movies:</h2>}
         {/* Mostra i risultati */}
-        {results.map((movie) => (
+        {movies.map((movie) => (
           <div key={movie.id}>
             <h2>{movie.title}</h2>
             <p>{movie.original_title}</p>
@@ -63,7 +74,50 @@ function App() {
             )}
 
 
-            <p>{movie.vote_count}</p>
+            <p>{movie.vote_average}</p>
+          </div>
+        ))}
+      </div>
+
+
+
+
+
+
+      {/* render series list */}
+
+      <div>
+        {series.length > 0 && <h2 className='list-title' >Series:</h2>}
+        {/* Mostra i risultati */}
+        {series.map((serie) => (
+          <div key={serie.id}>
+            <h2>{serie.name}</h2>
+            <p>{serie.original_name}</p>
+
+            {serie.original_language === 'en' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/gb.svg" alt="Flag of English" />
+            )}
+            {serie.original_language === 'it' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/it.svg" alt="Bandiera italiana" />
+            )}
+            {serie.original_language === 'es' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/es.svg" alt="Bandera española" />
+            )}
+            {serie.original_language === 'fr' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/fr.svg" alt="Drapeau français" />
+            )}
+            {serie.original_language === 'de' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/de.svg" alt="Deutsche Flagge" />
+            )}
+            {serie.original_language === 'ja' && (
+              <img className='flag' src="https://flagicons.lipis.dev/flags/4x3/jp.svg" alt="Deutsche Flagge" />
+            )}
+            {!['en', 'it', 'es', 'fr', 'de', 'ja'].includes(serie.original_language) && (
+              <p>{serie.original_language}</p>
+            )}
+
+
+            <p>{serie.vote_average}</p>
           </div>
         ))}
       </div>
